71f58f622616df2d51a4327c7292bc0c265ba458
Ulya Sh <ulyana.sh@ramotion.agency>
2021-11-08 19:19:55 +0300

    press asset template

---
+++ mozilla.design.2021/_partials/block-patterns/press-assets/press-asset.php
@@ -0,0 +1,18 @@
+<!-- wp:group {"className":"press-asset"} -->
+<div class="wp-block-group press-asset">
+  <!-- wp:cover {"url":"https://mozilla.design/files/2019/06/Master-Image-assets_noodle_bkgd_lockup_press-02.jpg"} -->
+  <div class="wp-block-cover has-background-dim">
+    <img class="wp-block-cover__image-background" alt="" src="https://mozilla.design/files/2019/06/Master-Image-assets_noodle_bkgd_lockup_press-02.jpg" data-object-fit="cover"/>
+    <div class="wp-block-cover__inner-container">
+      <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+      <p class="has-text-align-center has-large-font-size">Headers</p>
+      <!-- /wp:paragraph -->
+    </div>
+  </div>
+  <!-- /wp:cover -->
+
+  <!-- wp:paragraph {"className":"cta_text"} -->
+  <p class="cta_text"><a href="#">Download .zip</a></p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
