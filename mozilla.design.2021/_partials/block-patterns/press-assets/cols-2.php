e2f93ec7267b71f05251ef31dbf6841e57d36173
Ulya Sh <ulyana.sh@ramotion.agency>
2021-11-09 18:37:56 +0300

    add press assets group

--- mozilla.design.2021/_partials/block-patterns/press-assets/cols-2.php
+++
@@ -1,9 +0,0 @@
-<!-- wp:columns {"className":"row press-assets"} -->
-<div class="wp-block-columns row press-assets"><!-- wp:column {"className":"col-12 col-lg-6"} -->
-<div class="wp-block-column col-12 col-lg-6"></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"className":"col-12 col-lg-6"} -->
-<div class="wp-block-column col-12 col-lg-6"></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
--- mozilla.design.2021/_partials/block-patterns/press-assets/press-asset.php
+++ mozilla.design.2021/_partials/block-patterns/press-assets/press-asset.php
@@ -1,14 +1,11 @@
-<!-- wp:cover {"url":"https://mozilla.design/files/2019/06/press-office-1.jpg","dimRatio":0,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":0,"className":"press-asset"} -->
-<div class="wp-block-cover press-asset">
-  <img class="wp-block-cover__image-background" alt="" src="https://mozilla.design/files/2019/06/press-office-1.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/>
-  <div class="wp-block-cover__inner-container">
-    <!-- wp:heading {"textAlign":"left","textColor":"white","className":"type\u002d\u002dh-md caption"} -->
-    <h2 class="has-text-align-left type--h-md caption has-white-color has-text-color">Office Photos</h2>
-    <!-- /wp:heading -->
+<!-- wp:group {"className":"press-assets"} -->
+<div class="wp-block-group press-assets"><!-- wp:cover {"url":"https://mozilla.design/files/2019/06/press-office-1.jpg","dimRatio":0,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":0,"className":"press-asset"} -->
+<div class="wp-block-cover press-asset"><img class="wp-block-cover__image-background" alt="" src="https://mozilla.design/files/2019/06/press-office-1.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"left","textColor":"white","className":"type\u002d\u002dh-md caption"} -->
+<h2 class="has-text-align-left type--h-md caption has-white-color has-text-color">Office Photos</h2>
+<!-- /wp:heading -->
 
-    <!-- wp:paragraph {"textColor":"white","className":"text type\u002d\u002dsm"} -->
-    <p class="text type--sm has-white-color has-text-color"><a href="https://assets.mozilla.net/press/Mozilla_Offices.zip">Download .zip</a></p>
-    <!-- /wp:paragraph -->
-  </div>
-</div>
-<!-- /wp:cover -->
+<!-- wp:paragraph {"textColor":"white","className":"text type\u002d\u002dsm"} -->
+<p class="text type--sm has-white-color has-text-color"><a href="https://assets.mozilla.net/press/Mozilla_Offices.zip">Download .zip</a></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
